2a4cd75227e73e80063fcf6842f24d6cd118f1ea
fumiaos <158449969+fumiaos@users.noreply.github.com>
2026-01-06 10:49:40 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from format_bracket import *
 from format_th import convert_text
 from togeo import convert_to_pdb
 from totrain import to_train
-with open('total.xyz', encoding='utf-8') as f:
+with open('stru.xyz', encoding='utf-8') as f:
     data = f.read()
 data_lines = data.strip().split('\n')
 processed_lines = add_empty_line(data_lines)
@@ -17,5 +17,6 @@ train_output = to_train(output_text,num)
 print(train_output)
 with open('trainset.in', 'w', encoding='utf-8') as f:
     f.write(train_output)
+
 
 
